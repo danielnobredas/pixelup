754177f60a1346226774037e7f1692daf472fa6b
danielnobredas <danielnobre2007@hotmail.com>
2019-03-22 22:22:24 -0300

    Initial build

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { page: 'home' }
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

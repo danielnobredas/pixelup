9e116d16e34fe1bc3047572d097ea194731c7429
danielnobredas <danielnobre2007@hotmail.com>
2019-07-09 19:31:44 +0100

    Add: Team constants instead of teamdata variable for further use. Update: 'Ler' now links directly to the medium article.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { TEAM } from '../shared/team.constant';
 
 @Component({
   selector: 'app-about',
@@ -10,59 +11,7 @@ export class AboutComponent implements OnInit {
   constructor() {
   }
 
-  teamData = [
-    {
-      avatar: 'cayo.jpeg',
-      name: 'Cayo César',
-      twitter: 'cayocesar98',
-      instagram: 'cayocosta_',
-      facebook: '',
-      youtube: ''
-    }, {
-      avatar: 'clara.jpeg',
-      name: 'Clara Duque',
-      twitter: '',
-      instagram: 'duqueclara',
-      facebook: '',
-      youtube: 'UCQ7UiaS9zMzbfKMw0Sj3uKw'
-    }, {
-      avatar: 'daniel.jpeg',
-      name: 'Daniel Nobre',
-      twitter: '',
-      instagram: 'daniel.nobre',
-      facebook: '',
-      youtube: ''
-    }, {
-      avatar: 'davi.jpeg',
-      name: 'Davi Souto',
-      twitter: '',
-      instagram: 'davi_souto_',
-      facebook: '',
-      youtube: ''
-    }, {
-      avatar: 'jorge.jpeg',
-      name: 'Flávio Jorge',
-      twitter: 'jorge_deSantana',
-      instagram: 'jorge_dsantana',
-      facebook: '',
-      youtube: ''
-    }, {
-      avatar: 'lucca.jpeg',
-      name: 'Lucca Henrique',
-      twitter: '',
-      instagram: 'luccahen16',
-      facebook: '',
-      youtube: ''
-    }, {
-      avatar: 'paulo.jpeg',
-      name: 'Paulo Guerra',
-      quote: 'Eu juro solenemente não fazer nada de bom =)',
-      twitter: 'pv_guerra',
-      instagram: 'pv_guerra',
-      facebook: '',
-      youtube: ''
-    },
-  ];
+  teamData = TEAM;
 
   ngOnInit() {
   }
---
+++ src/app/shared/team.constant.ts
@@ -0,0 +1,52 @@
+export const TEAM = [{
+  avatar: 'cayo.jpeg',
+  name: 'Cayo Cesar',
+  twitter: 'cayocesar98',
+  instagram: 'cayocosta_',
+  facebook: '',
+  youtube: ''
+}, {
+  avatar: 'clara.jpeg',
+  name: 'Clara Duque',
+  twitter: '',
+  instagram: 'duqueclara',
+  facebook: '',
+  youtube: 'UCQ7UiaS9zMzbfKMw0Sj3uKw'
+}, {
+  avatar: 'daniel.jpeg',
+  name: 'Daniel Nobre',
+  quote: 'Science isn\'t about WHY, it\'s about WHY NOT?!',
+  twitter: '',
+  instagram: 'daniel.nobre',
+  facebook: '',
+  youtube: ''
+}, {
+  avatar: 'davi.jpeg',
+  name: 'Davi Souto',
+  twitter: '',
+  instagram: 'davi_souto_',
+  facebook: '',
+  youtube: ''
+}, {
+  avatar: 'jorge.jpeg',
+  name: 'Flávio Jorge',
+  twitter: 'jorge_deSantana',
+  instagram: 'jorge_dsantana',
+  facebook: '',
+  youtube: ''
+}, {
+  avatar: 'lucca.jpeg',
+  name: 'Lucca Henrique',
+  twitter: '',
+  instagram: 'luccahen16',
+  facebook: '',
+  youtube: ''
+}, {
+  avatar: 'paulo.jpeg',
+  name: 'Paulo Guerra',
+  quote: 'Eu juro solenemente não fazer nada de bom =)',
+  twitter: 'pv_guerra',
+  instagram: 'pv_guerra',
+  facebook: '',
+  youtube: ''
+}, ];
